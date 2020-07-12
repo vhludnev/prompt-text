00ee2d0ef187cc26b508bf4146824fb6223a088e
“Slava” <slavikman@yahoo.com>
2020-07-12 19:17:44 +0300

    1st

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let menu = document.getElementsByClassName("menu")[0],
+    menuItem = document.getElementsByClassName("menu-item"),
+    title = document.getElementById("title"),
+    adv = document.getElementsByClassName("adv")[0],
+    promptforApple = document.querySelector("#prompt"),
+    menuItemLi = document.createElement("li");
+
+menu.insertBefore(menuItem[2], menuItem[1]); // Поменяли местами два элемента (что ставим, куда ставим)
+
+menuItemLi.classList.add('menu-item');       // Добавляем новый li, с классом и текстом
+menuItemLi.textContent = 'Fifth';
+menu.appendChild(menuItemLi);
+
+document.body.style.backgroundImage = "url('img/apple_true.jpg')";   // Меняем фон
+
+title.textContent = "We only sell genuine Apple";     // Заменить текст
+
+adv.remove();   // Удалить рекламу со страницы
+
+let yourOpinion = prompt("What do you think about Apple?");   // Отношение к технике Apple
+promptforApple.textContent = yourOpinion;
